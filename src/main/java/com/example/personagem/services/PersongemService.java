203ceb9c54c9750ce8519440f020d9b720539c20
Robson <akaguikenta@gmail.com>
2025-04-08 23:28:14 -0300

    criando CRUD

--- src/main/java/com/example/personagem/services/PersongemService.java
+++ src/main/java/com/example/personagem/services/PersongemService.java
@@ -5,7 +5,11 @@ import com.example.personagem.repositories.PersonagemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PersongemService {
@@ -18,5 +22,15 @@ public class PersongemService {
         Personagem newPersonagem = personagemRepository.save(personagem);
         return ResponseEntity.ok(newPersonagem);
     }
+
+    public ResponseEntity<List<Personagem>> getAllPersonagems(){
+        List<Personagem> allPersonagems = personagemRepository.findAll();
+        return ResponseEntity.ok(allPersonagems);
+    }
+
+    public ResponseEntity<Optional<Personagem>> getPersonagemById(@PathVariable Long id){
+        Optional
+    }
+
 
 }
